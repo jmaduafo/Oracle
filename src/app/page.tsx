040e6585b291cb4092fd9b2cdb25c9719a299645
Jay Madu <jmaduafokwa@hotmail.com>
2024-03-03 20:38:29 +0100

    included video clip to about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+// 'use client'
 import Image from "next/image";
 import Hero from "@/components/Home/Hero";
 import About from "@/components/Home/About";
@@ -8,16 +8,16 @@ import Lenis from '@studio-freight/lenis'
 export default function Home() {
 
   // Lenis Smooth Scroll
-  useEffect(function() {
-    const lenis = new Lenis()
+//   useEffect(function() {
+//     const lenis = new Lenis()
     
-    function raf(time: number) {
-        lenis.raf(time)
-        requestAnimationFrame(raf)
-    }
+//     function raf(time: number) {
+//         lenis.raf(time)
+//         requestAnimationFrame(raf)
+//     }
     
-    requestAnimationFrame(raf)
-}, [])
+//     requestAnimationFrame(raf)
+// }, [])
 
   return (
     <main className="">
--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Image from 'next/image'
+import { reverie, satoshiMedium } from '@/styles/fonts'
+import star from '../../../public/images/smallStar.png'
 
 function About() {
   return (
-    <div>About</div>
+    <section className='relative overflow-y-hidden'>
+      <div className='relative'>
+        <div className='absolute top-0 left-0 w-full'>
+          <div className='flex items-center gap-6'>
+            <h3 className={`${satoshiMedium.className} tracking-tighter text-[5vw]`}>Lorem <span className={`${reverie.className} text-[5vw]`}>ipsum</span></h3>
+            <div className=''>
+              <Image
+                src={star}
+                alt='neon green star'
+              />
+            </div>
+          </div>
+        </div>
+        {/* mix-blend-color */}
+        <video className=' w-full mix-blend-color' autoPlay loop muted preload="none">
+          <source src="https://res.cloudinary.com/dyxxn831a/video/upload/v1709491474/oracle%20music/mula_vid_mp4_gl6cba.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </section>
   )
 }
 
