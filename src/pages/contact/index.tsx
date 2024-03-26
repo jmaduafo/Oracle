ae39af3a8b8d8596592ec48ce9bdb62355317423
Jay Madu <jmaduafokwa@hotmail.com>
2024-03-26 20:02:21 +0100

    cleaned up code

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -36,6 +36,10 @@ function Contact() {
       value: 'Referral',
       innerText: 'Referral'
     },
+    {
+      value: 'Join the team',
+      innerText: 'Want to join Oracle'
+    },
   ]
 
   const emailRegex = /[a-z0-9\._%+!$&*=^|~#%'`?{}/\-]+@([a-z0-9\-]+\.){1,}([a-z]{2,16})/
@@ -47,13 +51,11 @@ function Contact() {
       setError('No fields should be left empty')
     } else if (!email.match(emailRegex)) {
       setError('Invalid email address')
-    } else if (bookingType === '' || artist === '') {
+    } else if (bookingType === '') {
       setError('An option must be selected')
     } else {
       setError('')
     }
-
-    console.log(bookingType, artist)
   }
 
   return (
@@ -72,7 +74,9 @@ function Contact() {
 
         </div>
         <div className='flex-[2]'>
+          {/* FORM SUBMIT */}
           <form onSubmit={handleSubmit} className='mb-8 md:w-[90%]'>
+            {/* ERROR MESSAGE FOR FORM VALIDATION */}
             {error.length ?
             <div className='flex items-center justify-between rounded-xl border-[1px] border-accent px-6 py-3 mb-6'>
               <p className='text-white'>{error}</p>
@@ -81,22 +85,16 @@ function Contact() {
             :
             null
             }
+            {/* NAME INPUT */}
             <div className='flex flex-col mb-8'>
-              {/* <label htmlFor='formName' className='pl-3 mb-2 uppercase tracking-tight'>
-                Name *
-              </label> */}
               <input placeholder='Name *' className='placeholderForm text-[14px] px-3 py-2 outline-none border-b-white border-b-[1.5px] bg-transparent' onChange={(e) => setName(e.target.value)} id='formName' value={name} type='text'/>
             </div>
+            {/* EMAIL ADDRESS INPUT */}
             <div className='flex flex-col mb-8'>
-              {/* <label htmlFor='formEmail' className='pl-3 mb-2 uppercase tracking-tight'>
-                Email *
-              </label> */}
               <input placeholder='Email *' className='placeholderForm text-[14px] px-3 py-2 outline-none border-b-white border-b-[1.5px] bg-transparent' onChange={(e) => setEmail(e.target.value)} id='formEmail' value={email} type='email'/>
             </div>
+            {/* INQUIRY FOR REACHING OUT */}
             <div className='flex flex-col mb-8'>
-              {/* <label htmlFor='formInquiry' className='pl-3 mb-2 uppercase tracking-tight'>
-                What is your inquiry? *
-              </label> */}
               <select id='formInquiry' className='text-[14px] px-3 py-2 outline-none border-b-white border-b-[1.5px] bg-transparent' onChange={(e) => setBookingType(e.target.value)} value={bookingType}>
                 {
                   select.map((ele, index) => {
@@ -110,10 +108,8 @@ function Contact() {
                 }
               </select>
             </div>
+            {/* ARTIST THAT'S BEING ASKED FOR */}
             <div className='flex flex-col mb-8'>
-              {/* <label htmlFor='formInquiry' className='pl-3 mb-2 uppercase tracking-tight'>
-                Artist that you want to highlight? *
-              </label> */}
               <select id='formInquiry' className='placeholderForm px-3 py-2 outline-none border-b-white border-b-[1.5px] bg-transparent text-[14px]' onChange={(e) => setArtist(e.target.value)} value={artist}>
                 <option value='none' className='bg-background text-[14px]'>Which artist are you reaching out for?</option>
                 <option value='All Artists' className='bg-background text-[14px]'>All Artists</option>
@@ -121,17 +117,16 @@ function Contact() {
                   allArtists.map(ele => {
                     return (
                       <option key={ele.artistName} value={ele.artistName} className='bg-background text-[14px]'>{ele.artistName}</option>
-                    )
-                  })
-                }
+                      )
+                    })
+                  }
               </select>
             </div>
+            {/* MESSAGE TEXTAREA */}
             <div className='flex flex-col mb-8'>
-              {/* <label htmlFor='formMessage' className='pl-3 mb-2 uppercase tracking-tight'>
-                Message *
-              </label> */}
               <textarea placeholder='Send a Message *' id='formMessage' rows={4} className='placeholderForm text-[14px] px-3 py-2 outline-none border-b-white border-b-[1.5px] bg-transparent' onChange={(e) => setMessage(e.target.value)} value={message}></textarea>
             </div>
+            {/* SUBMIT BUTTON */}
             <div className='flex justify-center items-center mt-8'>
               <button type='submit' className={`group hover:text-background hover:bg-accent duration-[.4s] flex justify-between items-center gap-10 px-8 py-1 border-[1.5px] border-accent rounded-full uppercase text-[22px] text-white`}>
                 Submit
@@ -139,25 +134,9 @@ function Contact() {
               </button>
             </div>  
           </form>
-
         </div>
       </div>
     </section>
-  )
-}
-
-type FormLabel = {
-  label: string;
-  children: React.ReactNode;
-  formName: string;
-
-}
-function Label({ label, children, formName }: FormLabel ) {
-  return (
-    <label htmlFor={formName} className='flex flex-col'>
-      {label}
-      {children}
-    </label>
   )
 }
 
